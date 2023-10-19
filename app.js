b4013379e1f466bab84ce5de65ee6fd1cf2b574b
emilroy4 <emilroy100@gmail.com>
2023-10-19 15:51:35 +0100

    connecting to html

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html ' })
+    fs.readFile('index.html', function (error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error:File not found')
+        } else {
+            res.write(data)
+        }
+        res.end()
 
-
-
+    })
 })
 
-server.listen(port, function(error) {
-    if(error) {
+server.listen(port, function (error) {
+    if (error) {
         console.log('Something is wrong', error)
-    }   else {
+    } else {
         console.log('Server listening on port ' + port)
     }
 })
