686a69906303aeb9f084a7d4726aa1d014e6e2c8
emilroy4 <emilroy100@gmail.com>
2023-10-19 22:58:12 +0100

    setting up mongoDb 10.51

--- server.js
+++ server.js
@@ -1,50 +1,15 @@
 const express = require('express')
 const app = express()
-const bcrypt  = require('bcrypt')
-const users = []
+const expressLayouts = require('express-ejs-layouts')
 
+const indexRouter = require('./routes/index')
 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
 
+app.use('/', indexRouter    )
 
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-
-app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Emil' })
-})
-
-app.get('/login', (req, res) => {
- 
-    res.render('login.ejs')
-})
-app.post('/login', (req, res) => {
-    
-})
-
-
-
-
-app.get('/register', (req, res) => {
- 
-    res.render('register.ejs')
-})
-
-
-app.post('/register', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
-    } catch {
-        res.redirect('/register')
-    }
-        
-    
-})
-
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
