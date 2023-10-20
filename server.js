a983589c7c20c2e071fd11cb706758f248b2b7a0
emilroy4 <emilroy100@gmail.com>
2023-10-20 11:48:55 +0100

    database updat 16:04

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -10,6 +12,15 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-app.use('/', indexRouter    )
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, 
+    {useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose database'))
+
+
+app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)
